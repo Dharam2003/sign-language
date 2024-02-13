3732fa3defc9b3de7422fc777e68dee5f0349329
Dharam2003 <ss143mk@gmail.com>
2024-02-13 09:18:59 +0530

    fourth commit

--- app.py
+++ app.py
@@ -108,4 +108,5 @@ def index():
 @app.route('/video_feed')
 def video_feed():
     return Response(detect_sign_language(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
