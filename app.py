5c1d38f60c91604e8d9529d25fdde0b701a09983
Dharam2003 <ss143mk@gmail.com>
2024-02-15 22:19:38 +0530

    sixth commit

--- app.py
+++ app.py
@@ -109,4 +109,4 @@ def index():
 def video_feed():
     return Response(detect_sign_language(), mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0:8000')
